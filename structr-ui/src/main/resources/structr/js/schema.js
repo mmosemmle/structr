9a8d3863dfa630b8e0f44be06272053629964df3
vigorouscoding <kai@vigorouscoding.com>
2016-08-01 16:13:23 +0200

    reverted to previous relationship highlighting code since we reverted to previous display-style

--- structr-ui/src/main/resources/structr/js/schema.js
+++ structr-ui/src/main/resources/structr/js/schema.js
@@ -263,7 +263,7 @@ var _Schema = {
 
 		selectedRel = rel;
 		selectedRel.css({zIndex: ++maxZ});
-		selectedRel.next('._jsPlumb_overlay').css({zIndex: ++maxZ, borderColor: relHighlightColor, background: 'rgba(255, 255, 255, 1)'});
+		selectedRel.nextAll('._jsPlumb_overlay').slice(0, 3).css({zIndex: ++maxZ, borderColor: relHighlightColor, background: 'rgba(255, 255, 255, 1)'});
 		pathElements = selectedRel.find('path');
 		pathElements.css({stroke: relHighlightColor});
 		$(pathElements[1]).css({fill: relHighlightColor});
@@ -275,7 +275,7 @@ var _Schema = {
 
 		// deselect selected Relationship
 		if (selectedRel) {
-			selectedRel.next('._jsPlumb_overlay').css({borderColor:'', background: 'rgba(255, 255, 255, .8)'});
+			selectedRel.nextAll('._jsPlumb_overlay').slice(0, 3).css({borderColor:'', background: 'rgba(255, 255, 255, .8)'});
 			pathElements = selectedRel.find('path');
 			pathElements.css('stroke', '');
 			$(pathElements[1]).css('fill', '');
